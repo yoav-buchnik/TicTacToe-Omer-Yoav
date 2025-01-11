1f995425fe8cec1e24dde1107e6fa3d5ea564e07
unknown <yoavbu1003@gmail.com>
2025-01-11 18:25:21 +0200

    create tictactoe layout

---
+++ app/src/main/java/com/example/tictactoe_omer_yoav/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tictactoe_omer_yoav
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
